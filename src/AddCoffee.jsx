2ed11452a0849cb01cb8c728f1e19c3cc9f2ddc7
A.K.M Shah Alam <a.k.mshahala,68@gmail.com>
2023-12-04 20:06:55 +0600

    send data to backend

--- src/AddCoffee.jsx
+++ src/AddCoffee.jsx
@@ -1,9 +1,47 @@
+import Swal from 'sweetalert2'
+
 export default function AddCoffee() {
+
+  const handleAddCoffee = event =>{
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const quantity = form.quantity.value;
+    const supplier = form.supplier.value;
+    const taste = form.taste.value;
+    const category = form.category.value;
+    const details = form.details.value;
+    const photo = form.photo.value;
+    const newCoffee = {name,quantity,supplier,taste,category,details,photo}
+    console.log(newCoffee);
+
+    // send data to the server
+    fetch('http://localhost:5000/coffee',{
+      method: 'POST',
+      headers:{
+        'content-type':'application/json'
+      },
+      body:JSON.stringify(newCoffee)
+    })
+    .then(res =>res.json())
+    .then(data => {
+      console.log(data);
+      if(data.insertedId){
+        Swal.fire({
+          title: 'Success',
+          text: 'Coffee Added Successfully',
+          icon: 'success',
+          confirmButtonText: 'Cool'
+        })
+      }
+    })
+  }
+
   return (
     <div className="bg-[#F4F3F0] p-24">
       <h2 className="text-3xl font-extrabold ">Add a coffee</h2>
 
-      <form>
+      <form onSubmit={handleAddCoffee}>
         <div className="md:flex gap-4 mb-6">
           <div className="form-control md:w-1/2">
             <label className="label">
@@ -91,13 +129,13 @@ export default function AddCoffee() {
         <div className="">
           <div className="form-control w-full mb-6">
             <label className="label">
-              <span className="label-text">Coffee Name</span>
+              <span className="label-text">Photo</span>
             </label>
             <label className="input-group">
               <input
                 type="text"
-                name="name"
-                placeholder="Coffee Name"
+                name="photo"
+                placeholder="Enter photo URL"
                 className="input input-bordered md:w-full"
               />
             </label>
