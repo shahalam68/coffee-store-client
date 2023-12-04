33ef0c445eda93be96a59394b834b8b8c0db8b11
A.K.M Shah Alam <a.k.mshahala,68@gmail.com>
2023-12-04 13:23:04 +0600

    create from

--- src/AddCoffee.jsx
+++ src/AddCoffee.jsx
@@ -1,6 +1,112 @@
-
 export default function AddCoffee() {
   return (
-    <div>AddCoffee</div>
-  )
+    <div className="bg-[#F4F3F0] p-24">
+      <h2 className="text-3xl font-extrabold ">Add a coffee</h2>
+
+      <form>
+        <div className="md:flex gap-4 mb-6">
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text">Coffee Name</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                name="name"
+                placeholder="Enter coffee name"
+                className="input input-bordered md:w-full"
+              />
+            </label>
+          </div>
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text">Quantity</span>
+            </label>
+            <label className="input-group">
+              <input
+                name="quantity"
+                type="text"
+                placeholder="Quantity"
+                className="input input-bordered md:w-full"
+              />
+            </label>
+          </div>
+        </div>
+        <div className="md:flex gap-4 mb-6">
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text">Supplier</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                name="supplier"
+                placeholder="Enter coffee supplier"
+                className="input input-bordered md:w-full"
+              />
+            </label>
+          </div>
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text">Taste</span>
+            </label>
+            <label className="input-group">
+              <input
+                name="taste"
+                type="text"
+                placeholder="Enter coffee taste"
+                className="input input-bordered md:w-full"
+              />
+            </label>
+          </div>
+        </div>
+        <div className="md:flex gap-4 mb-6">
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text">Category</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                name="category"
+                placeholder="Enter coffee category"
+                className="input input-bordered md:w-full"
+              />
+            </label>
+          </div>
+          <div className="form-control md:w-1/2">
+            <label className="label">
+              <span className="label-text">Details</span>
+            </label>
+            <label className="input-group">
+              <input
+                name="details"
+                type="text"
+                placeholder="Enter coffee details"
+                className="input input-bordered md:w-full"
+              />
+            </label>
+          </div>
+        </div>
+        <div className="">
+          <div className="form-control w-full mb-6">
+            <label className="label">
+              <span className="label-text">Coffee Name</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="text"
+                name="name"
+                placeholder="Coffee Name"
+                className="input input-bordered md:w-full"
+              />
+            </label>
+          </div>
+        </div>
+        
+        <input type="submit" value="Add Coffee" className="btn btn-block bg-gray-900 text-white"  />
+
+      </form>
+    </div>
+  );
 }
